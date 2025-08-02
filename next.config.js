2ff54cc68c01498e18e9fdf727927a20314a87c3
MoosaKaif <164340042+Moosakaif@users.noreply.github.com>
2025-08-02 21:54:21 +0500

    sad

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.mjs
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output:'export',
-    images:{
-        unoptimized:true,
-    }
+// next.config.js
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
-
-export default nextConfig;
